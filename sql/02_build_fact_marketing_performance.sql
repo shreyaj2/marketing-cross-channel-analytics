4bd21b41ebfca1e67a7528539d19a45c194d5e0b
shreyaj2 <shreyajain27.sj@gmail.com>
2026-02-17 18:33:56 -0600

    create_unified_fact_table.sql

--- sql/02_build_fact_marketing_performance.sql
+++ sql/02_build_fact_marketing_performance.sql
@@ -1,1 +1,48 @@
+CREATE OR REPLACE TABLE `hale-photon-487521-p2.ads_demo.fact_ads` AS
+
+-- Facebook
+SELECT
+  date,
+  'Facebook' AS platform,
+  campaign_id,
+  campaign_name,
+  ad_set_id   AS ad_group_id,
+  ad_set_name AS ad_group_name,
+  impressions,
+  clicks,
+  spend,
+  conversions
+FROM `hale-photon-487521-p2.ads_demo.facebook_ads`
+
+UNION ALL
+
+-- Google
+SELECT
+  date,
+  'Google' AS platform,
+  campaign_id,
+  campaign_name,
+  ad_group_id,
+  ad_group_name,
+  impressions,
+  clicks,
+  cost AS spend,
+  conversions
+FROM `hale-photon-487521-p2.ads_demo.google_ads`
+
+UNION ALL
+
+-- TikTok
+SELECT
+  date,
+  'TikTok' AS platform,
+  campaign_id,
+  campaign_name,
+  adgroup_id   AS ad_group_id,
+  adgroup_name AS ad_group_name,
+  impressions,
+  clicks,
+  cost AS spend,
+  conversions
+FROM `hale-photon-487521-p2.ads_demo.tiktok_ads`;
 
